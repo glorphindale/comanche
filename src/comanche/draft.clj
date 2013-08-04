61d0c37e8c229fb2ea8c7ed47c404f43bd7733c5
Glorphindale <glorphindale@gmail.com>
2013-08-04 23:32:08 +0700

    Factor network layer out.

--- src/comanche/draft.clj
+++ src/comanche/draft.clj
@@ -1,16 +1,13 @@
 (ns comanche.draft
   (:require [taoensso.timbre :as timbre
-              :refer (debug info error)]
+             :refer (debug info error)]
+            [clojure.edn :as edn]
             [clojure.string :as string]
-            [clojure.edn :as edn])
-  (:import [org.jeromq ZMQ]))
+            [comanche.smoke :as smoke]))
 
 (timbre/set-config! [:appenders :spit :enabled?] true)
 (timbre/set-config! [:shared-appender-config :spit-filename] "logs.log")
 (timbre/set-config! [:current-level] :info)
-
-(def T 4000)
-(def ctx (ZMQ/context 1))
 
 ; Cluster generation
 (defn get-location [id]
@@ -26,86 +23,6 @@
 ;
 (def cluster
   (edn/read-string (slurp "cluster.conf")))
-
-; Transmission layer
-(defn send-msg
-  "Connect to a specified node, send message and read a response.
-  Msg must be in a number:message format"
-  ([my-id target-id msg]
-   (send-msg my-id target-id msg T))
-  ([my-id target-id msg timeout]
-   (let [target (:location (cluster target-id))]
-     (debug "Node" my-id ":" "Sending msg " msg " from " my-id " to " target-id ": " target)
-     (try
-       (with-open [sock (.socket ctx ZMQ/REQ)]
-         (.setReceiveTimeOut sock timeout)
-         (.setSendTimeOut sock timeout)
-         (.connect sock target)
-         (.send sock msg)
-         (String. (.recvStr sock)))
-       (catch Exception e :failure #_(do
-                                       (error "Node" my-id ":" "Exception when sending message " msg "to" target-id ":" e)
-                                       :failure))))))
-(defn make-msg [id msg]
-  (str id ":" msg))
-
-(defn split-msg [msg]
-  (let [[id text] (string/split msg #":")
-        out-id (Integer. id)]
-    [out-id text]))
-
-; Transport layer
-(defn send-msg-and-expect
-  ([my-id target-id in-msg out-msg]
-   (send-msg-and-expect my-id target-id in-msg out-msg T))
-  ([my-id target-id in-msg out-msg timeout]
-   (debug "Node" my-id ":" "send-msg-and-expect")
-   (let [msg (make-msg my-id in-msg)
-         exp-msg (make-msg target-id out-msg)
-         response (send-msg my-id target-id msg timeout)]
-     (cond (= response exp-msg) :ok
-           :else (do
-                   (info "Node" my-id ": Received" response "when waiting for" exp-msg)
-                   :failure)))))
-
-(defn split-cluster [pivot]
-  [(subvec cluster 0 pivot) (subvec cluster (inc pivot))])
-
-(defn ping-king [my-id king-id]
-  (debug "Node" my-id ":" "In ping king")
-  (send-msg-and-expect my-id king-id "PING" "PONG" (* 4 T)))
-
-(defn send-alive [my-id target-id]
-  (debug "Node" my-id ":" "Send alive" my-id ":" target-id)
-  (send-msg my-id target-id (make-msg my-id "ALIVE?")))
-
-(defn send-king [my-id target-id]
-  (send-msg-and-expect my-id target-id "IMTHEKING" "OK"))
-
-(defn broadcast-alive [my-id]
-  "Send ALIVE? requests in a futures and return them"
-  (debug "Node" my-id ":" "Broadcast alive")
-  (let [older-nodes (second (split-cluster my-id))]
-    (vec (map
-         (fn [older-node] (future (send-alive my-id (:id older-node))))
-         older-nodes))))
-
-(defn broadcast-king [my-id]
-  "Send IMTHEKING requests in a futures and return them"
-  (debug "Node" my-id ":" "Broadcasting kingness")
-  (let [younger-nodes (first (split-cluster my-id))]
-    (vec (map
-           (fn [younger-node] (future (send-king my-id (:id younger-node))))
-           younger-nodes))))
-
-(defn dig-broadcast [received searchee]
-  "Take result of broadcast and return ids of nodes, that replied with searchee"
-  (->> received
-       (map deref)
-       (filter #(not= % :failure))
-       (map split-msg)
-       (filter (fn [[_ msg]] (= msg searchee)))
-       (map (fn [[id _]] id))))
 
 ; State management
 (defn who-king? [knowledge]
@@ -139,28 +56,24 @@
 (defn ping [knowledge my-id]
   (debug "Node" my-id ":" "PING")
   (if-let [king-id (who-king? knowledge)]
-    (let [response (ping-king my-id king-id)]
+    (let [response (smoke/ping-king cluster my-id king-id)]
       (debug "Node" my-id ":" king-id "response is " response)
       (if (= response :failure)
         (king-lost! knowledge)))))
 
-(defn election-cycle [knowledge my-id]
-  (debug "Node" my-id ":" "new election cycle")
-  (let [broadcast-results (broadcast-alive my-id)
-        finethanks (dig-broadcast broadcast-results "FINETHANKS")
-        imtheking (dig-broadcast broadcast-results "IMTHEKING")]
-    (debug "Node" my-id ":" "Finethanks:" finethanks "imtheking:" imtheking)
-    (cond (not-empty imtheking) (king-found! knowledge (first imtheking))
-          (empty? finethanks) (do
-                                (broadcast-king my-id)
-                                (king-me! knowledge))
-          (not-empty finethanks) (do
-                                   (Thread/sleep T))
-          :else (debug "Node" my-id ":" "Election-cycle failed"))))
-
 (defn election [knowledge my-id]
   (while (no-king? knowledge)
-    (election-cycle knowledge my-id)))
+    (let [broadcast-results (smoke/broadcast-alive cluster my-id)
+          finethanks (smoke/dig-broadcast broadcast-results "FINETHANKS")
+          imtheking (smoke/dig-broadcast broadcast-results "IMTHEKING")]
+      (debug "Node" my-id ":" "Finethanks:" finethanks "imtheking:" imtheking)
+      (cond (not-empty imtheking) (king-found! knowledge (first imtheking))
+            (empty? finethanks) (do
+                                  (smoke/broadcast-king cluster my-id)
+                                  (king-me! knowledge))
+            (not-empty finethanks) (do
+                                     (Thread/sleep smoke/T))
+            :else (debug "Node" my-id ":" "Election-cycle failed")))))
 
 (defn state-loop [knowledge my-id]
   (while (not (exit? knowledge))
@@ -169,12 +82,12 @@
       (cond (no-king? knowledge) (election knowledge my-id)
             (stability? knowledge) (ping knowledge my-id)
             :else true))
-      (Thread/sleep T)))
+      (Thread/sleep smoke/T)))
 
 ; Receiving handler
 (defn transition [knowledge my-id in-msg]
   (debug "Node" my-id ": Transition" in-msg @knowledge)
-  (let [[sender-id msg] (split-msg in-msg)]
+  (let [[sender-id msg] (smoke/split-msg in-msg)]
     (cond
       (= msg "PING") "PONG"
       (= msg "IMTHEKING") (do
@@ -185,7 +98,7 @@
       (and (= msg "ALIVE?")
            (= my-id (dec (count cluster)))) (do
                                         (king-me! knowledge)
-                                        (broadcast-king my-id)
+                                        (smoke/broadcast-king cluster my-id)
                                         "IMTHEKING")
       (= msg "ALIVE?") (do
                          (king-lost! knowledge)
@@ -195,22 +108,7 @@
       :else "WAT?")))
 
 (defn receive-loop [knowledge my-id]
-  ; TODO kinda ugly function, should be refactored
-  (let [location (get-in cluster [my-id :location])
-        sock (.socket ctx ZMQ/REP)]
-    (try
-      (debug "Node" my-id ":" "Starting receive-loop")
-      (.bind sock location)
-      (.setSendTimeOut sock T)
-      (loop [in-msg (.recvStr sock)]
-        (debug "Node" my-id ":" "Received" in-msg)
-        (let [response (transition knowledge my-id in-msg)
-              out-msg (str my-id ":" response)]
-          (debug "Node" my-id ":" "Sending out" out-msg)
-          (.send sock out-msg))
-        (if (not (exit? knowledge))
-          (recur (.recvStr sock))))
-      (catch Exception e (debug "Node" my-id ": Caught exception" e)))))
+  (smoke/receive-func cluster my-id knowledge transition exit?))
 
 ; Glue them all
 (defn launch-node [id]
@@ -225,7 +123,10 @@
   (cond (empty? args) nil
         (some #{"-f"} args) (range (count cluster))
         (and (= (count args) 1) (re-find #"-" (first args)))
-          (let [[start stop] (map #(Integer. %) (string/split (first args) #"-"))] (range start (inc stop)))
+          (let [[start stop] (map #(Integer. %) (string/split (first args) #"-"))]
+            (if (< start stop)
+              (range start (inc stop))
+              nil))
         (every? #(< -1 (Integer. %) (count cluster)) args) (map #(Integer. %) args)
         :else nil))
 
---
+++ src/comanche/smoke.clj
@@ -0,0 +1,112 @@
+(ns comanche.smoke
+  (:require [taoensso.timbre :as timbre
+             :refer (debug info error)]
+            [clojure.string :as string])
+  (:import [org.jeromq ZMQ]))
+
+(def T 4000)
+
+(def ctx (ZMQ/context 1))
+
+(defn stop-zmq []
+  (.term ctx))
+
+; Transmission layer
+(defn send-msg
+  "Connect to a specified node, send message and read a response.
+  Msg must be in a number:message format"
+  ([cluster my-id target-id msg]
+   (send-msg cluster my-id target-id msg T))
+  ([cluster my-id target-id msg timeout]
+   (let [target (:location (cluster target-id))]
+     (debug "Node" my-id ":" "Sending msg " msg " from " my-id " to " target-id ": " target)
+     (try
+       (with-open [sock (.socket ctx ZMQ/REQ)]
+         (.setReceiveTimeOut sock timeout)
+         (.setSendTimeOut sock timeout)
+         (.setLinger sock timeout)
+         (.connect sock target)
+         (.send sock msg)
+         (String. (.recvStr sock)))
+       (catch Exception e (do
+                            (error "Node" my-id ":" "Exception when sending message " msg "to" target-id ":" e)
+                            :failure))))))
+(defn make-msg [id msg]
+  (str id ":" msg))
+
+(defn split-msg [msg]
+  (let [[id text] (string/split msg #":")
+        out-id (Integer. id)]
+    [out-id text]))
+
+; Transport layer
+(defn send-msg-and-expect
+  ([cluster my-id target-id in-msg out-msg]
+   (send-msg-and-expect cluster my-id target-id in-msg out-msg T))
+  ([cluster my-id target-id in-msg out-msg timeout]
+   (debug "Node" my-id ":" "send-msg-and-expect")
+   (let [msg (make-msg my-id in-msg)
+         exp-msg (make-msg target-id out-msg)
+         response (send-msg cluster my-id target-id msg timeout)]
+     (cond (= response exp-msg) :ok
+           :else (do
+                   (info "Node" my-id ": Received" response "when waiting for" exp-msg)
+                   :failure)))))
+
+(defn split-cluster [cluster pivot]
+  [(subvec cluster 0 pivot) (subvec cluster (inc pivot))])
+
+(defn ping-king [cluster my-id king-id]
+  (debug "Node" my-id ":" "In ping king")
+  (send-msg-and-expect cluster my-id king-id "PING" "PONG" (* 4 T)))
+
+(defn send-alive [cluster my-id target-id]
+  (debug "Node" my-id ":" "Send alive" my-id ":" target-id)
+  (send-msg cluster my-id target-id (make-msg my-id "ALIVE?")))
+
+(defn send-king [cluster my-id target-id]
+  (send-msg-and-expect cluster my-id target-id "IMTHEKING" "OK"))
+
+(defn broadcast-alive [cluster my-id]
+  "Send ALIVE? requests in a futures and return them"
+  (debug "Node" my-id ":" "Broadcast alive")
+  (let [older-nodes (second (split-cluster cluster my-id))]
+    (vec (map
+         (fn [older-node] (future (send-alive cluster my-id (:id older-node))))
+         older-nodes))))
+
+(defn broadcast-king [cluster my-id]
+  "Send IMTHEKING requests in a futures and return them"
+  (debug "Node" my-id ":" "Broadcasting kingness")
+  (let [younger-nodes (first (split-cluster cluster my-id))]
+    (vec (map
+           (fn [younger-node] (future (send-king cluster my-id (:id younger-node))))
+           younger-nodes))))
+
+(defn dig-broadcast [received searchee]
+  "Take result of broadcast and return ids of nodes, that replied with searchee"
+  (->> received
+       (map deref)
+       (filter #(not= % :failure))
+       (map split-msg)
+       (filter (fn [[_ msg]] (= msg searchee)))
+       (map (fn [[id _]] id))))
+
+(defn receive-func [cluster my-id knowledge transition-func exit-func]
+  (let [location (get-in cluster [my-id :location])
+        sock (.socket ctx ZMQ/REP)]
+    (try
+      (debug "Node" my-id ":" "Starting receive-loop")
+      (.bind sock location)
+      (.setSendTimeOut sock T)
+      (.setLinger sock T)
+      (loop [in-msg (.recvStr sock)]
+        (debug "Node" my-id ":" "Received" in-msg)
+        (let [response (transition-func knowledge my-id in-msg)
+              out-msg (str my-id ":" response)]
+          (debug "Node" my-id ":" "Sending out" out-msg)
+          (.send sock out-msg))
+        (if (not (exit-func knowledge))
+          (recur (.recvStr sock))))
+      (catch Exception e (debug "Node" my-id ": Caught exception" e)))))
+
