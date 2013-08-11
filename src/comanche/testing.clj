1db5fcf290916885e363a594d87e729e21807c4a
Glorphindale <glorphindale@gmail.com>
2013-08-11 22:14:41 +0700

    Delete old file.

--- src/comanche/testing.clj
+++
@@ -1,6 +0,0 @@
-(ns comanche.testing
-  (:require [taoensso.timbre :as timbre
-             :refer (trace debug info warn error fatal spy with-log-level)]))
-
-(info "NOOOOOO")
-(info (Exception. "EXCEPTION!!!" ))
